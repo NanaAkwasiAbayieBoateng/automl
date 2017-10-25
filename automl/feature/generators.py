dfef62d5a5634ff29f4488ab7674040a5011b81e
Igor Forkin <ifokin@cinimex.ru>
2017-10-25 20:19:52 +0300

    added polynomial features generator and tests

--- automl/feature/generators.py
+++ automl/feature/generators.py
@@ -22,10 +22,29 @@ class FeatureGeneratorBase(ABC):
 class PolynomialFeatureGenerator(FeatureGeneratorBase):
     """Class for polynomial features generators"""
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
     
-    def generate_features(self, X, **kwarg):
+    def generate_features(self, X, **kwargs):
         """
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            The data.
+        degree : integer
+            The degree of the polynomial features. Default = 2.
+        interaction_only : boolean, default = False
+            If true, only interaction features are produced: features that are
+            products of at most ``degree`` *distinct* input features (so not
+            ``x[1] ** 2``, ``x[0] * x[2] ** 3``, etc.).
+        include_bias : boolean
+            If True (default), then include a bias column, the feature in which
+            all polynomial powers are zero (i.e. a column of ones - acts as an
+            intercept term in a linear model).
+        Returns
+        -------
+        X_new : numpy array of shape [n_samples, n_features_new]
+            Transformed array.
+
         See Also
         --------
         http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.PolynomialFeatures.html
--- tests/test_features_generators.py
+++ tests/test_features_generators.py
@@ -1,9 +1,15 @@
 import unittest
 from unittest.mock import patch
 
-from automl.feature.generators import PolynomialFeatureGenerator
+from automl.feature.generators import PolynomialFeatureGenerator, FeatureGeneratorBase
 
 
 class TestPolynomialFeatureGenerator(unittest.TestCase):
-    def test_smth(self):
-        self.assertTrue(False)
+    @patch('sklearn.preprocessing.PolynomialFeatures.fit_transform')
+    def test_generate_features(self, fit_transform_mock):
+        X=[[1,2],[3,4]]
+        generator = PolynomialFeatureGenerator()
+        generator.generate_features(X)
+        fit_transform_mock.assert_called_with(X)
+
+    
