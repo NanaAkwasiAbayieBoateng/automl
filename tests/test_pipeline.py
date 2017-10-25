60e3b8d0c1742e0f4e3660f7268b419de16203b9
Kirill Dubovikov <dubovikov.kirill@gmail.com>
2017-10-25 17:26:05 +0300

    simplified pipeline creation in tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -3,9 +3,8 @@ from automl.pipeline import PipelineStep, Pipeline, LocalExecutor
 
 class TestPipeline(unittest.TestCase):
     def test_pipeline_step(self):
-        pipeline = Pipeline()
-        pipeline = pipeline >> PipelineStep('a', lambda x, context: x + 1) \
-                            >> PipelineStep('b', lambda x, context: x + 2) 
+        pipeline = Pipeline() >> PipelineStep('a', lambda x, context: x + 1) \
+                              >> PipelineStep('b', lambda x, context: x + 2) 
 
         executor = LocalExecutor()
         result = executor.run(pipeline, 0)
