560d2ae9a49bda62cca3d36352b20cc7a8549cde
Igor Forkin <ifokin@cinimex.ru>
2017-10-30 19:04:47 +0300

    merge conflicts

--- automl/feature/generators.py
+++ automl/feature/generators.py
@@ -4,6 +4,8 @@ import logging
 from abc import ABC, abstractmethod
 from sklearn.preprocessing import PolynomialFeatures
 from functools import partial
+import random
+import numpy as np
 
 
 class SklearnFeatureGenerator:
@@ -38,9 +40,52 @@ class SklearnFeatureGenerator:
 
 
 class FormulaFeatureGenerator:
-    """TODO Doc"""
-    def __init__(self, func_list=['+', '-']):
-        raise NotImplementedError()
+    def __init__(self):
+        """
+        Initialize Formula Feature Generator
 
+        Parameters
+        ----------
+        kwargs:
+            keyword arguments are passed to sklearn PolynomialFeatures
+        """
+        self._func_map = {
+            '+': self._sum,
+            '-': self._substract,
+            '/': self._divide,
+            '*': self._multiply,
+        }
 
+    def _sum(self, X):
+        x, y = self.choice_two_features(X)
+        print('sum')
+        return np.append(X, x+y, axis=1)
+    def _substract(self, X):
+        x, y = self.choice_two_features(X)
+        print('sub')
+        return np.append(X, x-y, axis=1)
+    def _divide(self, X):
+        x, y = self.choice_two_features(X)
+        print('div')
+        return np.append(X, x/y, axis=1)
+    def _multiply(self, X):
+        x, y = self.choice_two_features(X)
+        print('mul')
+        return np.append(X, x*y, axis=1)
+
+    def choice_two_features(self, X):
+        return X[:, random.randint(0, X.shape[1]-1)].reshape(X.shape[0], 1), \
+               X[:, random.randint(0, X.shape[1]-1)].reshape(X.shape[0], 1)
+
+    
+    def __call__(self, X, limit, pipeline_context):
+        if type(X) != np.ndarray:
+            print('sadfjsajdfhaklsjdhflk')
+            X = np.array(X)
+        print(type(X))
+        for i in range(0, limit):
+            X = self._func_map[random.choice(list(self._func_map))](X)
+        return X
+       
+        
 PolynomialGenerator = partial(SklearnFeatureGenerator, PolynomialFeatures)
