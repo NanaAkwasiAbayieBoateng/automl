added test on formula features generator and minor fixes

automl/feature/generators.py
@@ -160,10 +160,9 @@ class FormulaFeatureGenerator:
         XF : numpy array of shape [n_samples, n_features+limit]
             Transformed array.
         """
-        if X.isinstance(np.ndarray):
+        if not isinstance(X, np.ndarray):
             X = np.array(X)
-        print(type(X))
-        for i in range(0, limit):
+        for _ in range(0, limit):
             X = self._func_map[random.choice(list(self._func_map))](X)
         return X
 

tests/test_features_generators.py
@@ -1,9 +1,12 @@
 import unittest
-from unittest.mock import patch, MagicMock, Mock
+from unittest.mock import Mock
+import random
+
+from automl.feature.generators import SklearnFeatureGenerator, FormulaFeatureGenerator
+from automl.pipeline import PipelineContext
+
 import numpy as np
 
-from automl.feature.generators import SklearnFeatureGenerator
-from automl.pipeline import PipelineContext
 
 class TestSklearnFeatureGenerator(unittest.TestCase):
     def test_call_generator(self):
@@ -18,15 +21,19 @@ class TestSklearnFeatureGenerator(unittest.TestCase):
         gen(X, context)
         Transformer.fit_transform.assert_called_with(X)
 
-    def test_generate_features_kwargs(self):
+    def test_generate_polynomial_features_kwargs(self):
         Transformer = Mock()
 
-        kwargs = {
-            'degree': 3
-        }
-        
+        kwargs = {'degree': 3}
+
         transformer = lambda *args, **kwargs: Transformer(*args, **kwargs)
         gen = SklearnFeatureGenerator(transformer, **kwargs)
         Transformer.assert_called_with(**kwargs)
 
-
+    def test_generate_formula_feature(self):
+        a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        gen = FormulaFeatureGenerator()
+        limit = random.randint(0, 100)
+        context = PipelineContext()
+        self.assertEqual(
+            np.array(a).shape[1] + limit, gen(a, limit, context).shape[1])