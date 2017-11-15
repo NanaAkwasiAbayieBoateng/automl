77afca7669f4d6c612e2e19630e36f4528b1459e
Igor <ig.al.fokin@gmail.com>
2017-11-15 18:54:47 +0300

    fixed bug connected with saving trails of hyperopt

--- automl/hyperparam/hyperopt.py
+++ automl/hyperparam/hyperopt.py
@@ -7,7 +7,9 @@ from hyperopt import STATUS_OK
 class HyperparameterSearchResult:
     def __init__(self, best_model, best_score, history=None):
         self.best_model = best_model,
-        self.best_score = best_score
+        self.model = best_model,
+        self.best_score = best_score,
+        self.score = best_score
         self.history = history
 
 
@@ -20,22 +22,24 @@ class Hyperopt(ModelSpaceFunctor):
                 'loss': score_step_fn(*args, **kwargs).score,
                 'status': STATUS_OK 
                 }
-
+        
         self._max_evals = max_evals
-        self._trials = Trials()
 
     def __call__(self, pipeline_data, context):
+        trials = Trials()
         model, hparam_space = context
 
         score = partial(self._score_step_fn, pipeline_data, context)
+        #raise(ValueError)
         best = fmin(score,
                     space=hparam_space,
                     algo=tpe.suggest,
-                    trials=self._trials,
+                    trials=trials,
                     max_evals=self._max_evals)
-        best_loss = sorted(self._trials.losses())[0]
+        best_loss = sorted(trials.losses())[0]
+        print(f"BEST - {best}")
         result = HyperparameterSearchResult(best,
                                             best_loss,
-                                            self._trials)
+                                            trials)
         return result
 
--- tests/test_objects_as_piplinestep.py
+++ tests/test_objects_as_piplinestep.py
@@ -4,15 +4,19 @@ from automl.data.dataset import Dataset
 from automl.model import ModelSpace, Validate, CV, ChooseBest
 from automl.feature.selector import FeatureSelector
 from automl.feature.generators import FormulaFeatureGenerator
+from automl.hyperparam.hyperopt import Hyperopt
+from automl.hyperparam.templates import random_forest_hp_space, knn_hp_space, svc_hp_space, grad_boosting_hp_space
 
 from sklearn import datasets
 from sklearn.linear_model import LogisticRegression, Lasso, Ridge
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.metrics import accuracy_score, mean_absolute_error
+
+
 
 
 class TestSearchPipeline(unittest.TestCase):
@@ -64,6 +68,7 @@ class TestSearchPipeline(unittest.TestCase):
             (Lasso, {}),
             (Ridge, {}),
             (KernelRidge, {}),
+            (RandomForestRegressor, {})
         ]
 
         data = Dataset(datasets.load_boston().data, datasets.load_boston().target)
@@ -73,6 +78,31 @@ class TestSearchPipeline(unittest.TestCase):
             PipelineStep('cv', Validate(test_size=0.33, metrics=mean_absolute_error)) >>
             PipelineStep('choose', ChooseBest(3)) >>
             PipelineStep('selection', FeatureSelector(30)))
+
+        print('0'*30)
+        for result in pipeline_data.return_val:
+            print(result.model, result.score)
+        print(pipeline_data.dataset.data.shape)
+        print('0'*30)
+
+
+    def test_pipeline_hyperopt(self):
+        model_list = [
+            (RandomForestClassifier, random_forest_hp_space()),
+            #(GradientBoostingClassifier, grad_boosting_hp_space(lambda key: key)),
+            #(SVC, svc_hp_space('name')),
+            (KNeighborsClassifier, knn_hp_space(lambda key: key)),
+        ]
+
+        data = Dataset(datasets.load_iris().data, datasets.load_iris().target)
+        context, pipeline_data = LocalExecutor(data, 2) << (Pipeline() >> 
+            PipelineStep('model space', ModelSpace(model_list)) >>
+            #PipelineStep('feature generation', FormulaFeatureGenerator(['+', '-', '*'])) >>
+            PipelineStep('H', Hyperopt(Validate(test_size=0.33, metrics=mean_absolute_error), 
+                                                max_evals=10)) >>
+            PipelineStep('choose', ChooseBest(3)) 
+            # >> PipelineStep('selection', FeatureSelector(30))
+            )
 
         print('0'*30)
         for result in pipeline_data.return_val:
